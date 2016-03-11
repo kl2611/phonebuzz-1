9989cd3e61e15a0e90b1d7ec3e15350a5d07f5f3
Kelly <kelly.liu@columbia.edu>
2016-03-11 01:17:02 -0800

    changed request url

--- app.rb
+++ app.rb
@@ -2,15 +2,17 @@ require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
 
-get '/hello' do
+#PHONEBUZZ PHASE 1
+
+get '/' do
     Twilio::TwiML::Response.new do |r|
         r.Gather :finishOnKey => "#", :action => "/hello/phonebuzz", :method => 'get' do |g|
-            g.Say 'Please enter a number from 1 to 99 followed by the pound sign'
+            g.Say 'Enter a number from 1 to 99 followed by the pound sign'
         end
     end.text
 end
 
-get '/hello/phonebuzz' do
+get '/phonebuzz' do
   input = params['Digits'].to_i
   redirect '/hello' unless (input >= 1 && input <= 99)
   Twilio::TwiML::Response.new do |r|
