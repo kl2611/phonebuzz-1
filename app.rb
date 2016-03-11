c9abaa52f1e2900ddbf5bac6dd4b80ee52d7b55b
Kelly <kelly.liu@columbia.edu>
2016-03-10 23:56:24 -0800

    sms-quickstart

--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
 
-get '/hello-monkey' do
-  Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello Monkey'
-  end.text
+# get '/hello-monkey' do
+#   Twilio::TwiML::Response.new do |r|
+#     r.Say 'Hello Monkey'
+#   end.text
+# end
+
+get '/sms-quickstart' do
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message "Hey Monkey. Thanks for the message!"
+  end
+  twiml.text
 end
