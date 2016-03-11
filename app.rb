5eca03d273c1393804c474386a229237cedee0ab
Kelly <kelly.liu@columbia.edu>
2016-03-11 00:09:22 -0800

    fixed typo

--- app.rb
+++ app.rb
@@ -9,11 +9,12 @@ require 'twilio-ruby'
 # end
 
 get '/sms-quickstart' do
-  twiml = Twilio::TwiML::Response.new do |r|
-    r.Message "Hey Monkey. Thanks for the message!"
-    message.Body "Body"
-    message.Media "https://demo.twilio.com/owl.png"
-    message.Media "https://demo.twilio.com/logo.png"
-  end
-  twiml.text
+    twiml = Twilio::TwiML::Response.new do |r|
+        r.Message do |message|
+        message.Body "Body"
+        message.Media "https://demo.twilio.com/owl.png"
+        message.Media "https://demo.twilio.com/logo.png"
+        end
+    end
+    twiml.text
 end
