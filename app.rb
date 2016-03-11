89387a1493f3735ce16624948d637437a2a11a4a
Kelly <kelly.liu@columbia.edu>
2016-03-11 00:33:34 -0800

    fizzbuzz method

--- app.rb
+++ app.rb
@@ -10,9 +10,27 @@ require 'twilio-ruby'
 
 enable :sessions
 
-get '/sms-quickstart' do
+get '/hello' do
     Twilio::TwiML::Response.new do |r|
-        r.Say "Hello Monkey! You will get a text soon"
-        r.Sms "Here it is!"
+        r.Gather :finishOnKey => "#", :action => "/hello/phonebuzz", :method => 'get' do |s|
+            s.Say 'Please enter a number followed by the pound sign'
+        end
+    end.text
+end
+
+get '/hello/phonebuzz' do
+    input = params['Digits'].to_i
+    Twilio::TwiML::Response.new do |r|
+        input.times do |i|
+            if i % 15 == 0
+                r.Say = "fizzbuzz"
+            elsif i % 3 == 0
+                r.Say "fizz"
+            elsif i % 5 == 0
+                r.Say "buzz"
+            else
+                r.Say "#{i}"
+            end
+        end
     end.text
 end
