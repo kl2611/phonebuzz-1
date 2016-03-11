b43eacc2c3410ce4dd0bc3bd26f91ce5b0502e33
Kelly <kelly.liu@columbia.edu>
2016-03-11 00:07:34 -0800

    respond with mms

--- app.rb
+++ app.rb
@@ -11,6 +11,9 @@ require 'twilio-ruby'
 get '/sms-quickstart' do
   twiml = Twilio::TwiML::Response.new do |r|
     r.Message "Hey Monkey. Thanks for the message!"
+    message.Body "Body"
+    message.Media "https://demo.twilio.com/owl.png"
+    message.Media "https://demo.twilio.com/logo.png"
   end
   twiml.text
 end
