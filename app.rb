be72052afd7a1341154d88d942b8b9a7a4c8ed07
Kelly <kelly.liu@columbia.edu>
2016-03-11 00:58:59 -0800

    testing

--- app.rb
+++ app.rb
@@ -2,29 +2,31 @@ require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
 
-get '/hello' do
-    Twilio::TwiML::Response.new do |r|
-        r.Gather :finishOnKey => "#", :action => "/hello/phonebuzz", :method => 'get' do |s|
-            s.Say 'Please enter a number followed by the pound sign'
-        end
-    end.text
-end
+# get '/hello' do
+#     Twilio::TwiML::Response.new do |r|
+#         r.Gather :finishOnKey => "#", :action => "/hello/phonebuzz", :method => 'get' do |s|
+#             s.Say 'Please enter a number followed by the pound sign'
+#         end
+#     end.text
+# end
 
-get '/hello/phonebuzz' do
-    input = params['Digits'].to_i
-    redirect '/hello' unless (input >= 1 && input <= 999)
-    Twilio::TwiML::Response.new do |r|
-        input.times do |i|
-            current = i +
-            if current % 15 == 0
-                r.Say = "fizzbuzz"
-            elsif current % 3 == 0
-                r.Say "fizz"
-            elsif current % 5 == 0
-                r.Say "buzz"
-            else
-                r.Say "#{current}"
-            end
-        end
-    end.text
+get '/hello' do
+
+  # number = params['Digits'].to_i
+  number = 25
+  redirect '/hello' unless (number >= 1 && number <= 999)
+  Twilio::TwiML::Response.new do |r|
+    number.times do |i|
+      curr_num = i + 1
+      if curr_num % 15 == 0
+        r.Say "fizz buzz"
+      elsif curr_num % 5 == 0
+        r.Say "buzz"
+      elsif curr_num % 3 == 0
+        r.Say "fizz"
+      else
+        r.Say "#{curr_num}"
+      end
+    end
+  end.text
 end
