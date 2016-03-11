bd857c8d21b62d9654decf3ec3458afd482c4f3e
Kelly <kelly.liu@columbia.edu>
2016-03-11 00:14:37 -0800

    replies to incoming messages

--- app.rb
+++ app.rb
@@ -9,12 +9,13 @@ require 'twilio-ruby'
 # end
 
 get '/sms-quickstart' do
-    twiml = Twilio::TwiML::Response.new do |r|
-        r.Message do |message|
-        message.Body "Body"
-        message.Media "https://demo.twilio.com/owl.png"
-        message.Media "https://demo.twilio.com/logo.png"
-        end
-    end
-    twiml.text
+  sender = params[:From]
+  friends = {
+    "+13043765973" => "Kelly"
+  }
+  name = friends[sender] || "Mobile Monkey"
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message "Hello, #{name}. Thanks for the message."
+  end
+  twiml.text
 end
