1067e554852f84b8243a4d48d3721dd4d55dcd25
Kelly <kelly.liu@columbia.edu>
2016-03-11 17:39:26 -0800

    fixed files

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+<<<<<<< HEAD
+# PhoneBuzz
+
+This is a Sinatra app using Ruby and TwiML to play FizzBuzz over the phone.
+
+##Phase 1: Simple TwiML PhoneBuzz
+Direct your Twilio phone number to https://phonebuzz-1.herokuapp.com, and use HTTP GET request type. Call my demo Twilio number at +1 (347) 599-9364. A voice prompt will ask you to input a number between 1-99.
+
+##Phase 2: Dialing PhoneBuzz
+In progress
+
+##Phase 3: Delayed PhoneBuzz
+In progress
+
+##Phase 4: Tracking PhoneBuzz
+=======
 # Phonebuzz
 
 This is a Sinatra app using Ruby and TwiML to play FizzBuzz over the phone.
@@ -12,4 +28,5 @@ In progress
 In progress
 
 ##Phase 4
+>>>>>>> d885df32f92b3f49ebc5e7897d94f885d956b4f8
 In progress
--- app.rb
+++ app.rb
@@ -1,20 +1,65 @@
 require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
+require 'dotenv'
+
+Dotenv.load
 
 #PHONEBUZZ PHASE 1
 
+TWILIO_ACCOUNT_SID = 'AC3412f3a1f8d0868995248826459ce0ff'
+TWILIO_AUTH_TOKEN = '0bec4f2cef51865b25caabb53c604683'
+#@client = Twilio::REST::Client.new(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+# call = @client.calls.create(
+#     :url => "http://demo.twilio.com/docs/voice.xml",
+#     :to => "+13043765973",
+#     :from => "+15005550006") #Magic number
+# puts call.sid
+
+# def valid_request?
+#   validator = Twilio::Util::RequestValidator.new('0bec4f2cef51865b25caabb53c604683')
+#   uri = 'http://localhost:4567/'
+#   post_vars = {}
+#   signature = env['HTTP_X_TWILIO_SIGNATURE']
+#   if validator.validate uri, params, signature
+#     puts "Confirmed to have come from Twilio"
+#   else
+#     puts "NOT VALID"
+#   end
+# end
+
+# def invalid
+#   redirect '/error'
+# end
+
+# get 'error' do
+#   Twilio::TwiML::Response.new do |r|
+#       r.Say 'Invalid input'
+#   end.text
+# end
+
+# ['/', '/phonebuzz/*'].each do |path|
+#   before path do
+#     invalid unless valid_request?
+#   end
+# end
+
 get '/' do
+  erb :index
+end
+
+get '/phonebuzz' do
     Twilio::TwiML::Response.new do |r|
-        r.Gather :finishOnKey => "#", :action => "/phonebuzz", :method => 'get' do |g|
+        r.Gather :finishOnKey => "#", :action => "/phonebuzz/start", :method => 'get' do |g|
             g.Say 'Enter a number from 1 to 99 followed by the pound sign'
         end
     end.text
 end
 
-get '/phonebuzz' do
+get '/phonebuzz/start' do
   input = params['Digits'].to_i
-  redirect '/' unless (input >= 1 && input <= 99)
+  redirect '/phonebuzz' unless (input >= 1 && input <= 99)
   Twilio::TwiML::Response.new do |r|
     input.times do |i|
       count = i + 1
@@ -31,4 +76,12 @@ get '/phonebuzz' do
   end.text
 end
 
-
+post '/call' do
+  @client = Twilio::REST::Client.new(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+  call = @client.calls.create(
+      :url => "phonebuzz-1.herokuapp.com/phonebuzz",
+      :to => "+13043765973",
+      :from => "+15005550006",
+      :method => "get"
+    )
+end
